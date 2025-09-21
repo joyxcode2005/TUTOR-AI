3f5df5e9b7d266e87ae0ccdcf9781f9f390f6fb9
Joy Sengupta <joysengupta252005@gamil.com>
2025-09-21 19:25:34 +0530

    added Image and Link tags

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,64 +1,77 @@
 import InputField from "@/components/InputField";
 import Image from "next/image";
-
-
+import Link from "next/link";
 
 const page = () => {
   return (
     <div className="grid grid-cols-2">
       <div className="flex flex-col gap-5 justify-center items-start">
-
         <div className="flex items-center gap-3 absolute top-5 left-5">
           <img src="/images/logo.png" alt="" className="w-[30%]" />
-          <span className="font-bold text-[#073D44] text-2xl font-[productsans]">Tutor.AI</span>
+          <span className="font-bold text-[#073D44] text-2xl font-[productsans]">
+            Tutor.AI
+          </span>
         </div>
 
         <div className="w-full flex flex-col justify-center items-center gap-7 mt-16">
           <div className="w-full flex flex-col justify-center items-center mb-6">
             <div className="w-[60%] flex flex-col gap-2">
-              <h1 className="font-medium text-3xl font-[productsans]">Welcome Back!</h1>
-              <p className="text-[#8C8C8C] font-[productsans]">Sign in to access your dashboard and continue optimizing your learning process.
+              <h1 className="font-medium text-3xl font-[productsans]">
+                Welcome Back!
+              </h1>
+              <p className="text-[#8C8C8C] font-[productsans]">
+                Sign in to access your dashboard and continue optimizing your
+                learning process.
               </p>
             </div>
           </div>
-
 
           <form className="w-full flex flex-col gap-4 justify-center items-center">
             <InputField label="Email" placeholder="Enter your email" />
             <div className="flex flex-col w-full items-center gap-1">
               <InputField label="Password" placeholder="Enter Your Password" />
               <div className="w-[60%] flex justify-end">
-                <span className="text-[0.87rem] font-[productsans]">Don’t have an account? <a href="" className="font-bold text-[#03181a] ">Create now</a></span>
+                <span className="text-[0.87rem] font-[productsans]">
+                  Don’t have an account?{" "}
+                  <a href="" className="font-bold text-[#03181a] ">
+                    Create now
+                  </a>
+                </span>
               </div>
             </div>
 
-
-            <input type="submit" className="bg-[#073D44] w-[60%] h-12 rounded-[15px] text-white flex justify-center items-center font-[productsans]" value="Sign in" />
-
-
+            <input
+              type="submit"
+              className="bg-[#073D44] w-[60%] h-12 rounded-[15px] text-white flex justify-center items-center font-[productsans] hover:bg-[#073D44]/90 cursor-pointer transition-all duration-400 ease-linear hover:text-lg"
+              value="Sign in"
+            />
           </form>
 
-          <div className="flex justify-center items-center gap-3 w-full mt-7">
+          <div className="flex justify-center items-center gap-4 w-full mt-7">
             <div>
-              <img src="/images/Line_2.png" alt="" className="w-50" />
+              <Image src="/images/Line_2.png" alt="" width={40} height={40} />
             </div>
-            <span className="text-[#8C8C8C] font-medium font-[productsans]">Or</span>
+            <span className="text-[#8C8C8C] font-medium font-[productsans]">
+              Or
+            </span>
             <div>
-              <img src="/images/Line_1.png" alt="" className="w-50" /></div>
+              <Image src="/images/Line_1.png" alt="" width={40} height={40} />
+            </div>
           </div>
-
 
           <div className="flex justify-center w-full gap-5 mt-4">
-            <a href="#" className="flex justify-center items-center"><img src="/images/Google.png" alt="" className="w-[70%]" /></a>
-            <a href="#" className="flex justify-center items-center"><img src="/images/Facebook.png" alt="" className="w-[70%]" /></a>
-            <a href="#" className="flex justify-center items-center"><img src="/images/Apple.png" alt="" className="w-[70%]" /></a>
+            <Link href="#" className="flex justify-center items-center">
+              <Image src="/images/Google.png" alt="" width={40} height={40} />
+            </Link>
+            <Link href="#" className="flex justify-center items-center">
+              <Image src="/images/Facebook.png" alt="" width={40} height={40} />
+            </Link>
+            <Link href="#" className="flex justify-center items-center">
+              <Image src="/images/Apple.png" alt="" width={40} height={40} />
+            </Link>
           </div>
         </div>
-
-
       </div>
-
-
 
       <div className="flex flex-col items-center justify-center bg-[#073339] text-white p-12 h-screen relative overflow-hidden gap-12">
         <div className="absolute top-40 -right-60 blur-[100px] w-250 h-40 bg-gradient-to-b from-[#37787E] via-[#94dde3] to-[#5BCAD4] -rotate-45 z-10 rounded-2xl"></div>
--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -10,7 +10,7 @@ const InputField = ({ label, placeholder }: InputFieldProps) => {
   return (
     <div className='flex flex-col w-[60%] gap-2'>
       <label htmlFor={label} className='font-[productsans] text-[1.1rem]'>{label}</label>
-      <input id={label} placeholder={placeholder} className=' p-2 pl-5 rounded-[15px] bg-[#D9D9D9] text-[#9F9F9F] h-12 text-[0.85rem] font-[productsans]' />
+      <input id={label} placeholder={placeholder} className=' p-2 pl-5 rounded-[15px] bg-[#D9D9D9] text-[#9F9F9F] h-12 text-[0.85rem] font-[productsans] outline-[#073339]' />
     </div>
   )
 }
