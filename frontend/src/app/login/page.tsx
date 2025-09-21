83840049058271f28542a945a71a23268dd2e769
Abhirup Sarkar <jayitasarkar733134@gmail.com>
2025-09-21 13:03:36 +0530

    Login done

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -6,22 +6,60 @@ const page = () => {
     <div className="grid grid-cols-2">
       <div className="flex flex-col gap-5 justify-center items-start">
 
-      <div>
-          <h1 className="font-medium text-3xl">Welcome Back!</h1>
-          <p className="text-[#8C8C8C]">Sign in to access your dashboard and continue optimizing your learning process.
-          </p>
+        <div className="flex items-center gap-3 absolute top-5 left-5">
+          <img src="/images/logo.png" alt="" className="w-[30%]"/>
+          <span className="font-bold text-[#073D44] text-3xl">Tutor.AI</span>
+        </div>
+
+        <div className="w-full flex flex-col justify-center items-center gap-7 mt-16">
+          <div className="w-full flex flex-col justify-center items-center mb-6">
+            <div className="w-[60%]">
+              <h1 className="font-medium text-3xl">Welcome Back!</h1>
+              <p className="text-[#8C8C8C]">Sign in to access your dashboard and continue optimizing your learning process.
+              </p>
+            </div>
+          </div>
+
+
+          <form className="w-full flex flex-col gap-4 justify-center items-center">
+            <InputField label="Email" placeholder="Enter your email" />
+            <div className="flex flex-col w-full items-center gap-1">
+              <InputField label="Password" placeholder="Enter Your Password" />
+              <div className="w-[60%] flex justify-end">
+                <span className="text-[0.75rem]">Don’t have an account? <a href="" className="font-bold text-[#03181a]">Create now</a></span>
+              </div>
+            </div>
+
+
+            <input type="submit" className="bg-[#073D44] w-[60%] h-12 rounded-[15px] text-white flex justify-center items-center" value="Sign in" />
+
+
+          </form>
+
+          <div className="flex justify-center items-center gap-3 w-full mt-7">
+            <div>
+              <img src="/images/Line_2.png" alt="" className="w-50" />
+            </div>
+            <span className="text-[#8C8C8C] font-medium">Or</span>
+            <div>
+              <img src="/images/Line_1.png" alt="" className="w-50" /></div>
+          </div>
+
+
+          <div className="flex justify-center w-full gap-5 mt-4">
+            <a href="#" className="flex justify-center items-center"><img src="/images/Google.png" alt="" className="w-[70%]" /></a>
+            <a href="#" className="flex justify-center items-center"><img src="/images/Facebook.png" alt="" className="w-[70%]" /></a>
+            <a href="#" className="flex justify-center items-center"><img src="/images/Apple.png" alt="" className="w-[70%]" /></a>
+          </div>
+        </div>
+
+
       </div>
 
 
-       <div className="w-full flex flex-col gap-4">
-          <InputField label="Email" placeholder="Enter your email" />
-          <InputField label="Password" placeholder="Enter Your Password" />
-       </div>
 
-
-      </div>
-      <div className="flex flex-col items-center justify-center bg-[#073D44] text-white p-12 h-screen relative overflow-hidden gap-12">
-        <div className="absolute top-40 -right-60 blur-[100px] w-250 h-40 bg-gradient-to-b from-[#37787E] via-white to-[#5BCAD4] -rotate-45 z-10 rounded-2xl"></div>
+      <div className="flex flex-col items-center justify-center bg-[#073339] text-white p-12 h-screen relative overflow-hidden gap-12">
+        <div className="absolute top-40 -right-60 blur-[100px] w-250 h-40 bg-gradient-to-b from-[#37787E] via-[#94dde3] to-[#5BCAD4] -rotate-45 z-10 rounded-2xl"></div>
         <div className="flex flex-col items-center gap-2">
           <h2 className="text-4xl">
             Revolutionize Education With Smarter Automation
--- frontend/src/components/InputField.tsx
+++ frontend/src/components/InputField.tsx
@@ -8,9 +8,9 @@ interface InputFieldProps {
 
 const InputField = ({ label, placeholder }: InputFieldProps) => {
   return (
-    <div className='flex flex-col w-[50%] gap-2'>
+    <div className='flex flex-col w-[60%] gap-2'>
       <label htmlFor={label}>{label}</label>
-      <input id={label} placeholder={placeholder} className=' p-2 rounded-xl bg-[#D9D9D9] text-[#9F9F9F] h-12 text-[0.85rem]' />
+      <input id={label} placeholder={placeholder} className=' p-2 rounded-[15px] bg-[#D9D9D9] text-[#9F9F9F] h-12 text-[0.85rem]' />
     </div>
   )
 }
